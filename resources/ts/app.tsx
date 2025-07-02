3fde09e1cf09495c431738eacf68a3bc433fabb3
Santos Vilanculos <santosvilanculos@yahoo.com>
2025-07-03 01:14:22 +0200

    feat: get app name from the environment APP_NAME variable

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -1,9 +1,12 @@
+import { env } from '@/env';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 
+const { VITE_APP_NAME } = env;
+
 createInertiaApp({
-    title: title => title || 'Laravel',
+    title: title => title || VITE_APP_NAME,
     resolve: name => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
