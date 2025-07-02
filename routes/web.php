0efeba320469fc12e892bcd363a5dee35c8b1c7a
Santos Vilanculos <santosvilanculos@yahoo.com>
2025-07-03 00:48:00 +0200

    feat: add playground page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -9,3 +10,7 @@ Route::get('/', fn () => Inertia::render('dashboard', [
     'VERSION' => Illuminate\Foundation\Application::VERSION,
     'PHP_VERSION' => PHP_VERSION,
 ]))->name('dashboard');
+
+if (App::isLocal()) {
+    Route::get('/playground', fn () => Inertia::render('playground', []))->name('playground');
+}
