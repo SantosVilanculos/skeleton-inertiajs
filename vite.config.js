ff742b3ef788a66c4a0f9cb4ea40f40743e7bd68
Santos Vilanculos <santosvilanculos@yahoo.com>
2025-07-03 18:27:59 +0200

    feat(ide-helper): generate ide helper files automaticaly using vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import { run } from 'vite-plugin-run';
 
 export default defineConfig({
     plugins: [
@@ -10,7 +11,19 @@ export default defineConfig({
             refresh: true
         }),
         react(),
-        tailwindcss()
+        tailwindcss(),
+        run({
+            input: [
+                {
+                    build: false,
+                    run: ['php', 'artisan', 'ide-helper:generate']
+                },
+                {
+                    run: ['php', 'artisan', 'ide-helper:models', '-N'],
+                    pattern: ['./app/Models/**/*.php']
+                }
+            ]
+        })
     ],
     resolve: {
         alias: {
