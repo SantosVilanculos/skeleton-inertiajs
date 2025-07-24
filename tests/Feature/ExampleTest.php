29cc4048d35b9dd5591df9b586121e45222c3617
Santos Vilanculos <santosvilanculos@yahoo.com>
2025-07-25 00:44:37 +0200

    test: simplify response tests for clarity and maintainability

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,11 +3,23 @@
 declare(strict_types=1);
 
 use Illuminate\Http\Response;
+use Inertia\Testing\AssertableInertia as Assert;
 
-it('returns a successful response', function (string $path): void {
-    $this->get($path)->assertStatus(Response::HTTP_OK);
-})->with([
-    '/',
-    '/up',
-    '/log-viewer',
-]);
+test('up', function (): void {
+    $this->get('/up')->assertStatus(Response::HTTP_OK);
+});
+
+test('log-viewer', function (): void {
+    $this->get('/log-viewer')->assertStatus(Response::HTTP_OK);
+});
+
+test('dashboard', function () {
+    $response = $this->get('/');
+
+    $response->assertStatus(Response::HTTP_OK);
+
+    $response->assertInertia(fn (Assert $page) => $page
+        ->has('VERSION')
+        ->has('PHP_VERSION')
+    );
+});
